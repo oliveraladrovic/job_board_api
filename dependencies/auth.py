42038892680add674c8f4748f8ca8499b5050599
Oliver Aladrovic <oliver.aladrovic@gmail.com>
2026-01-16 20:06:20 +0100

    Changed auth protocol

--- dependencies/auth.py
+++ dependencies/auth.py
@@ -1,14 +1,15 @@
 from fastapi import Depends, HTTPException
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from database.base import get_db
 from models.users import User
 from services.jwt import decode_access_token
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+security = HTTPBearer()
 
-async def get_current_user(token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_db)) -> User:
+async def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security), db: AsyncSession = Depends(get_db)) -> User:
+    token = credentials.credentials
     user_id = decode_access_token(token)
     result = await db.execute(select(User).where(User.id == user_id))
     user = result.scalar_one_or_none()
